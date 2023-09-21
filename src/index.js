ea10a7e9f77f4dc65d6cf90b7d1779ebf62f8f72
Kuni <121762219+kuneus@users.noreply.github.com>
2023-09-20 23:30:48 -0400

    Add webpack and modules

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+// check if in production or development mode
+if (process.env.NODE_ENV === 'production') {
+  console.log('Production mode!');
+} else if (process.env.NODE_ENV === 'development') {
+  console.log('development mode!');
+}
+
+const knightMoves = (start, end) => {
+  //
+};
